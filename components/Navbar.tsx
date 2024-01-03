304a9983416320a743e85ecb33cf9c4ef3f1d472
MGS\laszlo.szekely <laszlo.szekely@magicsolutions.ro>
2024-01-03 15:34:37 +0200

    favicon and navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <header className='bg-red-200'>
+        <nav>
+            Nav
+        </nav>
+    </header>
+  )
+}
