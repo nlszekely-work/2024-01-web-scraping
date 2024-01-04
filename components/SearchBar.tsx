df9ebfa8a2e693d49e9efb4e124af9a93bbd8ae3
MGS\laszlo.szekely <laszlo.szekely@magicsolutions.ro>
2024-01-04 17:14:08 +0200

    url validation

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,27 +1,33 @@
 'use client'
 import React, { FormEvent, useState } from 'react'
 
-const isValidEmagUrl = (url: string) =>{
-    try{
+const isValidEmagUrl = (url: string) => {
+    try {
         const parsedUrl = new URL(url);
         const hostName = parsedUrl.hostname;
-        if(hostName.includes("emag.ro"))
-    }catch(error){
-
+        if (hostName.includes("emag.ro") || hostName.includes("emag.") || hostName.endsWith("emag")) {
+            return true
+        }
+    } catch (error) {
+        return false
     }
+    return false
+
 }
 
 const SearchBar = () => {
     const [searchPrompt, setSearchPrompt] = useState("");
-    const handleSubmit = (event: FormEvent<HTMLFormElement>) =>{
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
+        const isValidLink = isValidEmagUrl(searchPrompt);
+        alert(isValidLink)
     }
-  return (
-    <form onSubmit={handleSubmit}>
-        <input value={searchPrompt}  type='text' />
-        <button type='submit' />
-    </form>
-  )
+    return (
+        <form onSubmit={handleSubmit}>
+            <input onChange={(e)=> setSearchPrompt(e?.target?.value)} value={searchPrompt} type='text' />
+            <button type='submit' >Submit</button>
+        </form>
+    )
 }
 
 export default SearchBar
