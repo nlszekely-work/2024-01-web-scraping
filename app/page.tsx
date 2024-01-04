7959338c6f6a896a2e30ae279d5ca67f75ba6873
MGS\laszlo.szekely <laszlo.szekely@magicsolutions.ro>
2024-01-04 17:08:15 +0200

    searchbar

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         < >
             <section>
                 <SearchBar />
-                <HeroCarousel />
+                {/* <HeroCarousel /> */}
 
             </section>
             <section>
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+'use client'
+import React, { FormEvent, useState } from 'react'
+
+const isValidEmagUrl = (url: string) =>{
+    try{
+        const parsedUrl = new URL(url);
+        const hostName = parsedUrl.hostname;
+        if(hostName.includes("emag.ro"))
+    }catch(error){
+
+    }
+}
 
 const SearchBar = () => {
+    const [searchPrompt, setSearchPrompt] = useState("");
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) =>{
+        event.preventDefault();
+    }
   return (
-    <div>SearchBar</div>
+    <form onSubmit={handleSubmit}>
+        <input value={searchPrompt}  type='text' />
+        <button type='submit' />
+    </form>
   )
 }
 
