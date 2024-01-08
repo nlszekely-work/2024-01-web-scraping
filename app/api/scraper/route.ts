7fe28ddae08593842d6c71296e6e7e97d406641c
MGS\laszlo.szekely <laszlo.szekely@magicsolutions.ro>
2024-01-08 16:50:47 +0200

    stock check

--- app/api/scraper/route.ts
+++ app/api/scraper/route.ts
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 
 const targetClass = "product-new-price"
 const imageSrcTarget = "#product-gallery > div.ph-scroller > div > div:nth-child(1) > a > img"
+const isOutOfStockTargetClass = ".label-out_of_stock";
 
 export async function POST(request: Request, response: Response) {
     const { searchPrompt } = await request.json();
@@ -19,15 +20,25 @@ export async function POST(request: Request, response: Response) {
 
         const price = await page.$$eval(`.${targetClass}`, elements => {
             return elements.map(element => element.textContent);
-          });
+        });
+
+        //   Check availability
+        // <span class="label label-out_of_stock">Stoc epuizat</span>
+        let isOutOfStock: any = false
+        try {
+            isOutOfStock = await page.$eval(isOutOfStockTargetClass, element => element?.textContent);
+        } catch (err) {
+            isOutOfStock = false
+        }
 
         const imageSrc = await page.$eval(imageSrcTarget, img => img.src);
 
         await browser.close();
 
-        return NextResponse.json({ title: pageTitle, price:price[0],
-             image: imageSrc 
-            }, { status: 200 });
+        return NextResponse.json({
+            title: pageTitle, price: price[0],
+            image: imageSrc, isAvailable: !isOutOfStock
+        }, { status: 200 });
     } catch (error) {
         console.error('Error during scraping:', error);
         return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
