bf1705cc023353d8495926402e64a47829c0c4c1
nlszekely_work <nlszekely.work@gmail.com>
2024-01-11 21:45:26 +0200

    next config for sequlize

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        serverComponentsExternalPackages: ['sequelize', 'sequelize-typescript'],
+    }
+}
 
 module.exports = nextConfig
