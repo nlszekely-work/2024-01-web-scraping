b3c87ba3df97b60d023750afa91ba3146070af46
nlszekely_work <nlszekely.work@gmail.com>
2024-01-11 21:58:54 +0200

    sequlize ORM setup

--- app/api/scraper/route.ts
+++ app/api/scraper/route.ts
@@ -2,10 +2,34 @@ import puppeteer from "puppeteer";
 import { NextResponse } from "next/server";
 import sqlite3 from "sqlite3";
 import { open, Database } from "sqlite";
+import { DataTypes, Sequelize } from "sequelize";
 
 const targetClass = "product-new-price";
 const imageSrcTarget = "#product-gallery > div.ph-scroller > div > div:nth-child(1) > a > img";
 const isOutOfStockTargetClass = ".label-out_of_stock";
+
+const sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: "./db/database.db",
+});
+
+const ScrapedData = sequelize.define('ScrapedData', {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+    },
+    is_available: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+});
 
 export async function POST(request: Request, response: Response) {
     const { searchPrompt } = await request.json();
@@ -40,35 +64,48 @@ export async function POST(request: Request, response: Response) {
         // Save data to SQLite database -------------------------------------------
 
         // If the database instance is not initialized, open the database connection
-        const db = await open({
-            filename: "./collection.db", // Specify the database file path
-            driver: sqlite3.Database, // Specify the database driver (sqlite3 in this case)
+        // const db = await open({
+        //     filename: "./collection.db", // Specify the database file path
+        //     driver: sqlite3.Database, // Specify the database driver (sqlite3 in this case)
+        // });
+
+        // await db.exec(`
+        //   CREATE TABLE IF NOT EXISTS scraped_data (
+        //     id INTEGER PRIMARY KEY AUTOINCREMENT,
+        //     title TEXT,
+        //     price TEXT,
+        //     image TEXT,
+        //     is_available INTEGER
+        //   );
+        // `);
+
+        // const insertData = await db.prepare(`
+        //   INSERT INTO scraped_data (title, price, image, is_available)
+        //   VALUES (?, ?, ?, ?);
+        // `);
+
+        // await insertData.run(pageTitle, price[0], imageSrc, !isOutOfStock);
+        // await insertData.finalize();
+        // await db.close();
+
+        await ScrapedData.sync();
+
+        // Insert data into the table
+        const scrapedData = await ScrapedData.create({
+          title: pageTitle,
+          price: price[0],
+          image: imageSrc,
+          is_available: !isOutOfStock,
         });
 
-        await db.exec(`
-      CREATE TABLE IF NOT EXISTS scraped_data (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        title TEXT,
-        price TEXT,
-        image TEXT,
-        is_available INTEGER
-      );
-    `);
 
-        const insertData = await db.prepare(`
-      INSERT INTO scraped_data (title, price, image, is_available)
-      VALUES (?, ?, ?, ?);
-    `);
-
-        await insertData.run(pageTitle, price[0], imageSrc, !isOutOfStock);
-        await insertData.finalize();
-        await db.close();
         return NextResponse.json(
             {
-                title: pageTitle,
-                price: price[0],
-                image: imageSrc,
-                isAvailable: !isOutOfStock,
+                // title: pageTitle,
+                // price: price[0],
+                // image: imageSrc,
+                // isAvailable: !isOutOfStock,
+                hello: scrapedData,
             },
             { status: 200 }
         );
